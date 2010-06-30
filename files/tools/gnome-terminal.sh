63417c3ba0c65c553fa11b313d4a844e364ca9df
Ilya Averyanov <ilya.averyanov@gmail.com>
2010-06-30 14:39:40 +0400

    fix launcher scripts

--- files/tools/gnome-terminal.sh
+++ files/tools/gnome-terminal.sh
@@ -1,5 +1,7 @@
-#!/bin/sh
-# Usage: $0 [command]
+#!/bin/bash
+# Usage: $0 [tab title] [command]
+title=$1
+shift
 pgrep -u "$USER" gnome-terminal | grep -qvx "$$"
 if [ "$?" -eq 0 ]; then
   WID=`xdotool search --class "gnome-terminal" | head -1`
@@ -7,22 +9,15 @@ if [ "$?" -eq 0 ]; then
   xdotool key ctrl+shift+t
   
   sleep .1
-  # You could use, instead of this clipboard hack:
   # xdotool type "$1"
   # xdotool key Return
 
-  # save old clipboard value
   oldclip="$(xclip -o)"
   
-  if [ "$#" -ne 0 ] ; then
-    # Use copy+paste to write the command to the shell. This is
-    # slightly more reliable than 'xdotool type'
-    echo "exec sh -c '$*'" | xclip -i 
-    xdotool key shift+Insert
-  fi
+  echo "echo -e \"\\x1B]2;$title\\x07\"; $*" | xclip -i 
+  xdotool key shift+Insert
 
-  # Restore old clipboard value
   echo "$oldclip" | xclip -i
 else
-  /usr/bin/gnome-terminal -e "sh -c '$*'"
+  gnome-terminal -e "$*" --title="$title"
 fi
