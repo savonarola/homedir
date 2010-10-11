d71678a3283861e5773141eca8732ad9ab5376ec
Ilya Averyanov <ilya.averyanov@gmail.com>
2010-10-11 12:46:17 +0400

    git branch in bash path

--- files/tools/bash_prompt.pl
+++ files/tools/bash_prompt.pl
@@ -3,8 +3,11 @@ use strict;
 
 use IPC::Open3 qw(open3);
 
-my $pid = open3(\*CHLD_IN, \*CHLD_OUT, \*CHLD_ERR, qw(git  br));
-my $output  = <CHLD_OUT>;
+my $pid = open3(\*CHLD_IN, \*CHLD_OUT, \*CHLD_ERR, 'git', 'br');
+my $output;
+while(<CHLD_OUT>) {
+    $output = $_ if /^\*/;
+}
 close CHLD_IN;
 close CHLD_OUT;
 close CHLD_ERR;
