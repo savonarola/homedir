05bfd51a3bae142fc92153c206faafc73d8cab42
Ilya Averyanov <ilya.averyanov@gmail.com>
2011-12-20 16:06:07 +0400

    better prompt

---
+++ configs/zshrc/51_prompt.sh
@@ -0,0 +1,1 @@
+PROMPT='%{$fg_bold[red]%}%n%{$fg_bold[white]%}@%{$fg_bold[green]%}%m ➜ %{$fg_bold[green]%}%p %{$fg[cyan]%}%c %{$fg_bold[blue]%}$(git_prompt_info)%{$fg_bold[blue]%} % %{$reset_color%}'
