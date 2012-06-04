6e7d36bf11ce6ba2d0776e8d7467629b01409a11
av <av@fun-box.ru>
2012-06-04 15:42:28 +0400

    app commands

--- configs/zshrc/23_users.sh
+++ configs/zshrc/23_users.sh
@@ -2,3 +2,10 @@ alias f="sudo -u fbdeploy"
 alias fs="f sudo -u service"
 alias fsh="sudo -E -u fbdeploy zsh"
 alias fssh="sudo -E -u fbdeploy sudo -E -u service"
+
+alias fp="f env RAILS_ENV=production"
+alias fsp="fs env RAILS_ENV=production"
+
+alias fpb="fp bundle exec"
+alias fspb="fsp bundle exec"
+
