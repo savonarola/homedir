4a7de5dbcf8c7fca10d414c9c636c13cf90d8062
av <av@fun-box.ru>
2015-06-24 23:03:27 +0300

    deploy aliases && vsn script refactoring

---
+++ configs/zshrc/52_deploy.sh
@@ -0,0 +1,4 @@
+alias dp="vsn patch bundle exec cap production release:deploy"
+alias dmn="vsn minor bundle exec cap production release:deploy"
+alias dmd="vsn middle bundle exec cap production release:deploy"
+alias dmj="vsn major bundle exec cap production release:deploy"
