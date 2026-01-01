4a92db7eab6d7d4c02de792d7ec71467f1ed6644
Ilia Averianov <av@rubybox.dev>
2026-01-02 01:30:56 +0200

    improve tmux and vim settings

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -1,1 +1,13 @@
 require "nvchad.autocmds"
+
+-- Restore cursor position when opening a file
+vim.api.nvim_create_autocmd("BufReadPost", {
+  pattern = "*",
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -2,5 +2,7 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
+local o = vim.o
+o.swapfile = false
+
 -- o.cursorlineopt ='both' -- to enable cursorline!
