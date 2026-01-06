3ff0454772e300615f5cc36a5913a25a119fc1ef
Ilia Averianov <av@rubybox.dev>
2026-01-06 02:17:34 +0200

    add git blame plugin

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -145,4 +145,9 @@ return {
     },
   },
 
+  {
+    "Yu-Leo/blame-column.nvim",
+    opts = {}, -- for default options. Refer to the configuration section for custom setup.
+    cmd = "BlameColumnToggle",
+  },
 }
