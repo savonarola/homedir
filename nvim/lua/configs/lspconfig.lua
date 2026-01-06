11d5d675cf39d6ebc7a4a01ba8fd7053bc6583bb
Ilia Averianov <av@rubybox.dev>
2026-01-06 02:19:02 +0200

    setup diagnostics

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -6,4 +6,23 @@ local servers = {
 }
 vim.lsp.enable(servers)
 
+vim.diagnostic.config({
+  virtual_text = {
+    severity = { min = vim.diagnostic.severity.WARN },
+  },
+  signs = {
+    severity = { min = vim.diagnostic.severity.INFO },
+  },
+  float = {
+    severity = { min = vim.diagnostic.severity.HINT },
+  },
+  underline = {
+    severity = { min = vim.diagnostic.severity.WARN },
+  },
+  severity = {
+    min = vim.diagnostic.severity.WARN,
+  },
+})
+
+
 -- read :h vim.lsp.config for changing options of lsp servers 
