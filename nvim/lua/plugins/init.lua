a28970c02dae0e181f97121aa8590e9647f34e9b
Ilia Averianov <av@rubybox.dev>
2026-05-06 12:15:08 +0300

    remove flash

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -64,73 +64,10 @@ return {
     end,
   },
 
-  -- Flash - navigate with search labels
-  {
-    "folke/flash.nvim",
-    event = "VeryLazy",
-    opts = {
-      modes = {
-        search = {
-          enabled = true,
-        },
-        char = {
-          enabled = false,
-        },
-      },
-    },
-    keys = {
-      {
-        "s",
-        mode = { "n", "x", "o" },
-        function()
-          require("flash").jump()
-        end,
-        desc = "Flash",
-      },
-      {
-        "S",
-        mode = { "n", "x", "o" },
-        function()
-          require("flash").treesitter()
-        end,
-        desc = "Flash Treesitter",
-      },
-      {
-        "r",
-        mode = "o",
-        function()
-          require("flash").remote()
-        end,
-        desc = "Remote Flash",
-      },
-      {
-        "R",
-        mode = { "o", "x" },
-        function()
-          require("flash").treesitter_search()
-        end,
-        desc = "Treesitter Search",
-      },
-      {
-        "<c-s>",
-        mode = { "c" },
-        function()
-          require("flash").toggle()
-        end,
-        desc = "Toggle Flash Search",
-      },
-    },
-  },
-
   {
     "Yu-Leo/blame-column.nvim",
     opts = {}, -- for default options. Refer to the configuration section for custom setup.
     cmd = "BlameColumnToggle",
   },
-
-  -- {
-  --   "Mofiqul/vscode.nvim",
-  --   opts = {},
-  -- }
 
 }
